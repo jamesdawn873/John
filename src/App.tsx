278ad277ac029562ec08c914558a6b6e8085ac63
jamesdawn873 <johnh20@uci.edu>
2024-12-21 17:52:06 -0800

    changed NavBar to use props instead of hard coded elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import NavBar from "./components/NavBar";
 
 function App() {
+    let items = [
+        'John',
+        'Projects'
+    ]
+    let links = [
+        'LinkedIn',
+        'GitHub'
+    ]
     return (
-        <><NavBar/></>
+        <><NavBar leftItems={items} rightItems={links} /></>
+
     )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,22 @@
-import { Fragment } from "react";
+interface navBarProps {
+    leftItems: string[];
+    rightItems: string[];
+}
 
-function NavBar(){
-    const items = [
-        'John',
-        'GitHub',
-        'LinkedIn',
-        'Projects'
-    ]
+function NavBar({leftItems, rightItems}: navBarProps) {
 
     return (
-        <ul className = "flex">
-            {items.map(item =>
-                <li className="float-left p-4" key={item}>{item}</li>
-            )}
-        </ul>
-        
+        <div className="max-w-7xl mx-auto my-3.5">
+            <ul>
+                <li className="float-left"></li>
+                {leftItems.map(item =>
+                    <li className="hover:bg-green-100 float-left px-4 py-2 rounded-md" key={item}>{item}</li>
+                )}
+                {rightItems.map(item =>
+                    <li className={"hover:bg-green-100 float-right px-4 py-2 rounded-md"} key={item}>{item}</li>)}
+            </ul>
+        </div>
+
     )
 }
 
