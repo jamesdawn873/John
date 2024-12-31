bba320aaa6096df45f85f2d268be24074b1d5dba
jamesdawn873 <johnh20@uci.edu>
2024-12-31 01:39:30 -0800

    added Main.tsx for main functionality

---
+++ src/components/Main.tsx
@@ -0,0 +1,18 @@
+import {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+                <div className={"columns-2 border-x-auto flex justify-center mx-6 p-3"}>
+                    <p className={"p-2"}>
+                        hellosaasdasd
+                    </p>
+                    <p className={"p-2"}>
+                        hello2asdasa
+                    </p>
+                </div>
+        );
+    }
+}
+
+export default Main;
