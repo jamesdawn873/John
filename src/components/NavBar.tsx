c07eeb09973ac53fed9eddb3d6eec32d6299e5c6
jamesdawn873 <johnh20@uci.edu>
2024-12-31 01:43:09 -0800

    changed <ul> to <div> changed <li> to <a>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,15 +6,10 @@ interface navBarProps {
 function NavBar({leftItems, rightItems}: navBarProps) {
 
     return (
-        <div className="max-w-7xl mx-auto my-3.5">
-            <ul>
-                <li className="float-left"></li>
-                {leftItems.map(item =>
-                    <li className="hover:bg-green-100 float-left px-4 py-2 rounded-md" key={item}>{item}</li>
-                )}
-                {rightItems.map(item =>
-                    <li className={"hover:bg-green-100 float-right px-4 py-2 rounded-md"} key={item}>{item}</li>)}
-            </ul>
+        <div className="max-w-7xl mx-auto my-3.5 content-center p-2 bg-green-100" id={"navbar"}>
+            <a href={"/"} className="hover:bg-green-100 float-left px-4 py-2 rounded-md" >John</a>
+            {rightItems.map(item =>
+                <a className={"hover:bg-green-100 float-right px-4 py-2 rounded-md"}    key={item}>{item}</a>)}
         </div>
 
     )
